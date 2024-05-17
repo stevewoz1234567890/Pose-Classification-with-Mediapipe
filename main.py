615170315fd89a348b7b85495c673184c6650369
stevewoz1234567890 <87572213+stevewoz1234567890@users.noreply.github.com>
2024-05-17 07:55:49 -0700

    main update

--- main.py
+++ main.py
@@ -21,11 +21,39 @@ from mediapipe.python.solutions import drawing_utils as mp_drawing
 from mediapipe.python.solutions import pose as mp_pose
 import argparse
 
+# Create ArgumentParser object
+parser = argparse.ArgumentParser()
+
+# Add argument
+parser.add_argument("--path", help="Path argument")
+parser.add_argument("--pose", help="Pose Type argument")
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+
+# Access the value of the argument
+if args.path:
+    print("Video Path:", args.path)
+    video_path = args.path
+else:
+    print("Video Path not provided.")
+      
+if args.pose:
+    print("Pose Type:", args.pose)
+    class_name=args.pose
+else:
+    print("Pose Type not provided.")  
+
+
 def show_image(img, figsize=(10, 10)):
   """Shows output PIL image."""
   plt.figure(figsize=figsize)
   plt.imshow(img)
   plt.show()
+
+pose_classification_history = []
+pose_classification_filtered_history = []
 
 class FullBodyPoseEmbedder(object):
   """Converts 3D pose landmarks into 3D embedding."""
@@ -819,6 +847,63 @@ class BootstrapHelper(object):
           if not n.startswith('.')])
       print('  {}: {}'.format(pose_class_name, n_images))
 
+bootstrap_images_in_folder = 'fitness_poses_images_in'
+
+# Output folders for bootstrapped images and CSVs.
+bootstrap_images_out_folder = 'fitness_poses_images_out'
+bootstrap_csvs_out_folder = 'fitness_poses_csvs_out'
+
+# Initialize helper.
+bootstrap_helper = BootstrapHelper(
+    images_in_folder=bootstrap_images_in_folder,
+    images_out_folder=bootstrap_images_out_folder,
+    csvs_out_folder=bootstrap_csvs_out_folder,
+)
+
+# Check how many pose classes and images for them are available.
+bootstrap_helper.print_images_in_statistics()
+
+# Bootstrap all images.
+# Set limit to some small number for debug.
+bootstrap_helper.bootstrap(per_pose_class_limit=None)
+
+# Check how many images were bootstrapped.
+bootstrap_helper.print_images_out_statistics()
+
+# After initial bootstrapping images without detected poses were still saved in
+# the folderd (but not in the CSVs) for debug purpose. Let's remove them.
+#bootstrap_helper.align_images_and_csvs(print_removed_items=False)
+#bootstrap_helper.print_images_out_statistics()
+
+# Align CSVs with filtered images.
+#bootstrap_helper.align_images_and_csvs(print_removed_items=False)
+#bootstrap_helper.print_images_out_statistics()
+
+# Find outliers.
+
+# Transforms pose landmarks into embedding.
+pose_embedder = FullBodyPoseEmbedder()
+
+# Classifies give pose against database of poses.
+pose_classifier = PoseClassifier(
+    pose_samples_folder=bootstrap_csvs_out_folder,
+    pose_embedder=pose_embedder,
+    top_n_by_max_distance=30,
+    top_n_by_mean_distance=10)
+
+outliers = pose_classifier.find_pose_sample_outliers()
+print('Number of outliers: ', len(outliers))
+
+# Analyze outliers.
+bootstrap_helper.analyze_outliers(outliers)
+
+# Remove all outliers (if you don't want to manually pick).
+bootstrap_helper.remove_outliers(outliers)
+
+# Align CSVs with images after removing outliers.
+#bootstrap_helper.align_images_and_csvs(print_removed_items=False)
+#bootstrap_helper.print_images_out_statistics()
+
 def dump_for_the_app():
   pose_samples_folder = 'fitness_poses_csvs_out'
   pose_samples_csv_path = 'fitness_poses_csvs_out.csv'
@@ -843,222 +928,139 @@ def dump_for_the_app():
 
 #files.download(pose_samples_csv_path)
 
+dump_for_the_app()
 
 ##########################################################################
 #####################  classification ####################################
 ##########################################################################
-if __name__ == '__main__':
-  # Create ArgumentParser object
-  parser = argparse.ArgumentParser()
-
-  # Add argument
-  parser.add_argument("--path", help="Path argument")
-  parser.add_argument("--pose", help="Pose Type argument")
-
-  # Parse the command-line arguments
-  args = parser.parse_args()
 
 
-  # Access the value of the argument
-  if args.path:
-      print("Video Path:", args.path)
-      video_path = args.path
-  else:
-      print("Video Path not provided.")
-        
-  if args.pose:
-      print("Pose Type:", args.pose)
-      class_name=args.pose
-  else:
-      print("Pose Type not provided.")  
 
-  pose_classification_history = []
-  pose_classification_filtered_history = []
+# open video
+video_cap = cv2.VideoCapture(video_path)
 
-  bootstrap_images_in_folder = 'fitness_poses_images_in'
+# Get some video parameters to generate output video with classificaiton.
+video_n_frames = video_cap.get(cv2.CAP_PROP_FRAME_COUNT)
+video_fps = video_cap.get(cv2.CAP_PROP_FPS)
+video_width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+video_height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-  # Output folders for bootstrapped images and CSVs.
-  bootstrap_images_out_folder = 'fitness_poses_images_out'
-  bootstrap_csvs_out_folder = 'fitness_poses_csvs_out'
+out_video_path = video_path.split('.')[0] + '_out.' + video_path.split('.')[1]
 
-  # Initialize helper.
-  bootstrap_helper = BootstrapHelper(
-      images_in_folder=bootstrap_images_in_folder,
-      images_out_folder=bootstrap_images_out_folder,
-      csvs_out_folder=bootstrap_csvs_out_folder,
-  )
+# Initialize tracker.
+pose_tracker = mp_pose.Pose() #upper_body_only=False
 
-  # Check how many pose classes and images for them are available.
-  bootstrap_helper.print_images_in_statistics()
+# Initialize embedder.
+pose_embedder = FullBodyPoseEmbedder()
 
-  # Bootstrap all images.
-  # Set limit to some small number for debug.
-  bootstrap_helper.bootstrap(per_pose_class_limit=None)
+# Initialize classifier
+pose_samples_folder = 'fitness_poses_csvs_out' #Folder with pose class CSVs
+pose_classifier = PoseClassifier(
+    pose_samples_folder=pose_samples_folder,
+    pose_embedder=pose_embedder,
+    top_n_by_max_distance=30,
+    top_n_by_mean_distance=10)
 
-  # Check how many images were bootstrapped.
-  bootstrap_helper.print_images_out_statistics()
+# Uncomment to validate target poses used by classifier and find outliers.
+outliers = pose_classifier.find_pose_sample_outliers()
+# print('Number of pose sample outliers (consider removing them): ', len(outliers))
 
-  # After initial bootstrapping images without detected poses were still saved in
-  # the folderd (but not in the CSVs) for debug purpose. Let's remove them.
-  #bootstrap_helper.align_images_and_csvs(print_removed_items=False)
-  #bootstrap_helper.print_images_out_statistics()
+# Initialize EMA smoothing.
+pose_classification_filter = EMADictSmoothing(
+    window_size=10,
+    alpha=0.2)
 
-  # Align CSVs with filtered images.
-  #bootstrap_helper.align_images_and_csvs(print_removed_items=False)
-  #bootstrap_helper.print_images_out_statistics()
+# Initialize counter.
+repetition_counter = RepetitionCounter(
+    class_name=class_name,
+    enter_threshold=4.7,
+    exit_threshold=4.7)
 
-  # Find outliers.
+# Initialize renderer.
+pose_classification_visualizer = PoseClassificationVisualizer(
+    class_name=class_name,
+    plot_x_max=video_n_frames,
+    # Graphic looks nicer if it's the same as `top_n_by_mean_distance`.
+    plot_y_max=10)
 
-  # Transforms pose landmarks into embedding.
-  pose_embedder = FullBodyPoseEmbedder()
+### Run classification on a video.
 
-  # Classifies give pose against database of poses.
-  pose_classifier = PoseClassifier(
-      pose_samples_folder=bootstrap_csvs_out_folder,
-      pose_embedder=pose_embedder,
-      top_n_by_max_distance=30,
-      top_n_by_mean_distance=10)
+# Open output video.
+out_video = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*'mp4v'), video_fps, (video_width, video_height))
 
-  outliers = pose_classifier.find_pose_sample_outliers()
-  print('Number of outliers: ', len(outliers))
+frame_idx = 0
+output_frame = None
+with tqdm.tqdm(total=video_n_frames, position=0, leave=True) as pbar:
+  while True:
+    # Get next frame of the video.
+    success, input_frame = video_cap.read()
+    if not success:
+      break
 
-  # Analyze outliers.
-  bootstrap_helper.analyze_outliers(outliers)
+    # Run pose tracker.
+    input_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
+    result = pose_tracker.process(image=input_frame)
+    pose_landmarks = result.pose_landmarks
 
-  # Remove all outliers (if you don't want to manually pick).
-  bootstrap_helper.remove_outliers(outliers)
+    # Draw pose prediction.
+    output_frame = input_frame.copy()
+    if pose_landmarks is not None:
+      mp_drawing.draw_landmarks(
+          image=output_frame,
+          landmark_list=pose_landmarks,
+          connections=mp_pose.POSE_CONNECTIONS)
+    
+    if pose_landmarks is not None:
+      # Get landmarks.
+      frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
+      pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
+                                for lmk in pose_landmarks.landmark], dtype=np.float32)
+      assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
 
-  # Align CSVs with images after removing outliers.
-  #bootstrap_helper.align_images_and_csvs(print_removed_items=False)
-  #bootstrap_helper.print_images_out_statistics()
+      # Classify the pose on the current frame.
+      pose_classification = pose_classifier(pose_landmarks)
 
-  dump_for_the_app()
+      # Smooth classification using EMA.
+      pose_classification_filtered = pose_classification_filter(pose_classification)
 
-  # open video
-  video_cap = cv2.VideoCapture(video_path)
+      # Count repetitions.
+      repetitions_count = repetition_counter(pose_classification_filtered)
+    else:
+      # No pose => no classification on current frame.
+      pose_classification = None
 
-  # Get some video parameters to generate output video with classificaiton.
-  video_n_frames = video_cap.get(cv2.CAP_PROP_FRAME_COUNT)
-  video_fps = video_cap.get(cv2.CAP_PROP_FPS)
-  video_width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-  video_height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+      # Still add empty classification to the filter to maintaing correct
+      # smoothing for future frames.
+      pose_classification_filtered = pose_classification_filter(dict())
+      pose_classification_filtered = None
 
-  out_video_path = video_path.split('.')[0] + '_out.' + video_path.split('.')[1]
+      # Don't update the counter presuming that person is 'frozen'. Just
+      # take the latest repetitions count.
+      repetitions_count = repetition_counter.n_repeats
 
-  # Initialize tracker.
-  pose_tracker = mp_pose.Pose() #upper_body_only=False
+    # Draw classification plot and repetition counter.
+    output_frame = pose_classification_visualizer(
+        frame=output_frame,
+        pose_classification=pose_classification,
+        pose_classification_filtered=pose_classification_filtered,
+        repetitions_count=repetitions_count)
 
-  # Initialize embedder.
-  pose_embedder = FullBodyPoseEmbedder()
+    # Save the output frame.
+    out_video.write(cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
 
-  # Initialize classifier
-  pose_samples_folder = 'fitness_poses_csvs_out' #Folder with pose class CSVs
-  pose_classifier = PoseClassifier(
-      pose_samples_folder=pose_samples_folder,
-      pose_embedder=pose_embedder,
-      top_n_by_max_distance=30,
-      top_n_by_mean_distance=10)
+    # Show intermediate frames of the video to track progress.
+    # if frame_idx % 50 == 0:
+    #   show_image(output_frame)
 
-  # Uncomment to validate target poses used by classifier and find outliers.
-  outliers = pose_classifier.find_pose_sample_outliers()
-  # print('Number of pose sample outliers (consider removing them): ', len(outliers))
+    frame_idx += 1
+    pbar.update()
 
-  # Initialize EMA smoothing.
-  pose_classification_filter = EMADictSmoothing(
-      window_size=10,
-      alpha=0.2)
+# Close output video.
+out_video.release()
 
-  # Initialize counter.
-  repetition_counter = RepetitionCounter(
-      class_name=class_name,
-      enter_threshold=4.7,
-      exit_threshold=4.7)
+# Release MediaPipe resources.
+pose_tracker.close()
 
-  # Initialize renderer.
-  pose_classification_visualizer = PoseClassificationVisualizer(
-      class_name=class_name,
-      plot_x_max=video_n_frames,
-      # Graphic looks nicer if it's the same as `top_n_by_mean_distance`.
-      plot_y_max=10)
-
-  ### Run classification on a video.
-
-  # Open output video.
-  out_video = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*'mp4v'), video_fps, (video_width, video_height))
-
-  frame_idx = 0
-  output_frame = None
-  with tqdm.tqdm(total=video_n_frames, position=0, leave=True) as pbar:
-    while True:
-      # Get next frame of the video.
-      success, input_frame = video_cap.read()
-      if not success:
-        break
-
-      # Run pose tracker.
-      input_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
-      result = pose_tracker.process(image=input_frame)
-      pose_landmarks = result.pose_landmarks
-
-      # Draw pose prediction.
-      output_frame = input_frame.copy()
-      if pose_landmarks is not None:
-        mp_drawing.draw_landmarks(
-            image=output_frame,
-            landmark_list=pose_landmarks,
-            connections=mp_pose.POSE_CONNECTIONS)
-      
-      if pose_landmarks is not None:
-        # Get landmarks.
-        frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
-        pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
-                                  for lmk in pose_landmarks.landmark], dtype=np.float32)
-        assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
-
-        # Classify the pose on the current frame.
-        pose_classification = pose_classifier(pose_landmarks)
-
-        # Smooth classification using EMA.
-        pose_classification_filtered = pose_classification_filter(pose_classification)
-
-        # Count repetitions.
-        repetitions_count = repetition_counter(pose_classification_filtered)
-      else:
-        # No pose => no classification on current frame.
-        pose_classification = None
-
-        # Still add empty classification to the filter to maintaing correct
-        # smoothing for future frames.
-        pose_classification_filtered = pose_classification_filter(dict())
-        pose_classification_filtered = None
-
-        # Don't update the counter presuming that person is 'frozen'. Just
-        # take the latest repetitions count.
-        repetitions_count = repetition_counter.n_repeats
-
-      # Draw classification plot and repetition counter.
-      output_frame = pose_classification_visualizer(
-          frame=output_frame,
-          pose_classification=pose_classification,
-          pose_classification_filtered=pose_classification_filtered,
-          repetitions_count=repetitions_count)
-
-      # Save the output frame.
-      out_video.write(cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
-
-      # Show intermediate frames of the video to track progress.
-      # if frame_idx % 50 == 0:
-      #   show_image(output_frame)
-
-      frame_idx += 1
-      pbar.update()
-
-  # Close output video.
-  out_video.release()
-
-  # Release MediaPipe resources.
-  pose_tracker.close()
-
-  # # Show the last frame of the video.
-  # if output_frame is not None:
-  #   show_image(output_frame)
+# # Show the last frame of the video.
+# if output_frame is not None:
+#   show_image(output_frame)
